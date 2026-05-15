c19438dca8a709e8433b3381e386d5c5278502f0
Rajibbepary <rajib635356@gmail.com>
2026-05-15 06:35:35 +0600

    vertical-timeline-element-content css change

--- components/Education.jsx
+++ components/Education.jsx
@@ -16,25 +16,26 @@ const Education = () => {
       <h1 className="text-center text-4xl font-bold mb-4">My Journey</h1>
 
       <VerticalTimeline>
-       
-         <VerticalTimelineElement
-          date="2020 - 2023"
-          contentStyle={{
-            background: "#F3E8FF",
-            color: "#6A0DAD",
-          }}
-          iconStyle={{
-            background: "#6A0DAD",
-            color: "#fff",
-          }}
-        
-        >
-          <h3 className="text-xl font-bold">
-            Bachelor of Computer Science
-          </h3>
+        <VerticalTimelineElement
+  date="2020 - 2023"
+  contentStyle={{
+    background: "#1D293D",
+    color: "#fff",
+    transition: "all 0.4s ease",
+  }}
+  contentArrowStyle={{ borderRight: "7px solid #1D293D" }}
+  className="vertical-timeline-element--work custom-timeline"
+  iconStyle={{
+    background: "#6A0DAD",
+    color: "#fff",
+  }}
+>
+  <h3 className="text-xl font-bold">
+    Bachelor of Computer Science
+  </h3>
 
-          <p>B.Sc in Computer Science & Engineering</p>
-        </VerticalTimelineElement>
+  <p>B.Sc in Computer Science & Engineering</p>
+</VerticalTimelineElement>
       </VerticalTimeline>
     </div>
   );
