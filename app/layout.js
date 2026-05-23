e1607cc8d4b2690cce27c8868416a2d5ff17970a
Rajibbepary <rajib635356@gmail.com>
2026-05-23 07:41:34 +0600

    error fix

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ const ovo = Ovo({
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html  lang="en" suppressHydrationWarning>
             <body className="font-Outfit leading-8 dark:bg-darkTheme dark:text-white">
                 {children}
             <Toaster position="top-right" richColors />
